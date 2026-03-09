019d9a2ba54e0abf288fe74ae52fa1f626b8a026
henas_pl <kontakt@alleria.pl>
2026-03-09 17:11:36 +0100

    Initial Commit

---
+++ Gatevision/GatevisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatevisionApp.swift
+//  Gatevision
+//
+//  Created by Mateusz Fronc on 09/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct GatevisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
